e6e6cb04c03285b63ee49f0a497d5f35ca5f47aa
Gonzalo <gonzalo@safetymind.ai>
2024-12-18 16:37:10 -0300

    sube cambios en grua 101

--- captura/captura_camara.py
+++ captura/captura_camara.py
@@ -9,6 +9,8 @@ import signal
 import configparser
 import pytz
 import cv2
+import logging
+from PIL import Image,ImageDraw,ImageStat
 
 def limpiar(dir,ult_file):
     for file in os.listdir(dir):
@@ -18,25 +20,25 @@ def limpiar(dir,ult_file):
 
 def procesar(config):
     
-  
     username = config["CAMARA"]["username"]
     password = config["CAMARA"]["password"]
     url = config["CAMARA"]["url"]
 
-    ruta_base_descarga=config["CAPTURA"]["ruta_base_descarga"]
-    frameskip=config["CAPTURA"]["frameskip"]
-    tiempo_espera_error=config["CAPTURA"]["tiempo_espera_error"]
-    nombre_camara = config["CAPTURA"]["nombre_camara"]
+    ruta_descarga=config["CAPTURA"]["ruta_descarga"]
+    frameskip=config.getint("CAPTURA","frameskip")
+    tiempo_espera_error=config.getint("CAPTURA","tiempo_espera_error")
+    nombre_camara=config["CAPTURA"]["nombre_camara"]
     
     hora_america=pytz.timezone('America/Lima')
 
     indice=1
 
-    vidcap = cv2.VideoCapture(self.url_rtsp,cv2.CAP_FFMPEG)
+    #vidcap = cv2.VideoCapture(url,cv2.CAP_FFMPEG)
+    vidcap = cv2.VideoCapture(url)
 
     if (vidcap.isOpened() == False):
-        print("no se puede abrir archivo: "+self.url_rtsp)
-    return
+        print("no se puede abrir archivo: "+url)
+        return
 
     while (vidcap.isOpened()):
         try:
@@ -47,12 +49,12 @@ def procesar(config):
                 
             nombre_imagen=texto_fecha+'-'+texto_hora+"-"+nombre_camara+".jpg"
 
-            ruta_captura=ruta_base_descarga+"/"+nombre_camara+"/"+nombre_imagen
+            ruta_captura=ruta_descarga+"/"+nombre_imagen
 
             ret, frame = vidcap.read()
 
-            if not self.frame_skip ==0:
-                for z in range(self.frame_skip):
+            if not frameskip ==0:
+                for z in range(frameskip):
                     ret, frame = vidcap.read()
 
             if ret == False:
@@ -82,14 +84,14 @@ def procesar(config):
         except Exception as e:
             logging.exception("Error en Factory Stream mp4")
             print(e)
-            time.sleeptiempo_espera_error
+            time.sleep(tiempo_espera_error)
 
 def signal_handler(key, frame):
 	print("\n[*] Saliendo\n")
 	sys.exit(1)
 
 if __name__ == "__main__":
-    
+    print("iniciando")
     signal = signal.signal(signal.SIGINT, signal_handler)
 
     parser = argparse.ArgumentParser(description='Pipeline puente grua')
