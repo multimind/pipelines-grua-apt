a38053193fdbf1f73c504dea681d4bc3be336461
Gonzalo <gonzalo@safetymind.ai>
2024-12-21 11:32:13 -0300

    sube cambios asociados a pull

--- procesamiento/operadores/descarga/factoryStreamPullCarpeta.py
+++ procesamiento/operadores/descarga/factoryStreamPullCarpeta.py
@@ -78,6 +78,7 @@ class FactoryStreamPullCarpeta:
                         "factor_riesgo":"no"
                     }
                     print("aca???")
+                    print(json)_datos
                     observer.on_next(json_datos)
                     print("sigo")
 
