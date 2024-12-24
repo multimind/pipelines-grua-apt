c49cbc136191882dcfe167bfb06956f30eab9bed
Gonzalo <gonzalo@safetymind.ai>
2024-12-24 11:21:43 -0300

    sube cambios asociados a captura gstreamer

--- captura/captura_gstreamer.py
+++ captura/captura_gstreamer.py
@@ -14,13 +14,14 @@ import os
 import pika 
 from systemd.daemon import notify
 
-STATUS_FILE = "/run/apt_descarga/apt-descarga.status"
+STATUS_FILE = None
 ruta_frames=None
 nombre_canal=None
 
 def on_new_sample(sink,user_data):
     global ruta_frames
-    
+    global STATUS_FILE
+
     print("==inicio")
     channel=user_data
 
@@ -91,8 +92,6 @@ def on_new_sample(sink,user_data):
         nombre_final="frames/"+nombre_captura+".jpg"
         image.save(nombre_final)
 
-        #ruta_frames="/data/pipelines-grua-apt/captura/"
-
         print("antes de publicar!!!")
         channel.basic_publish(exchange='', routing_key='imagen_grua_apt', body=ruta_frames+nombre_final)
         print("publicado!")
@@ -107,6 +106,7 @@ def on_new_sample(sink,user_data):
 
     print("==fin normal")
     
+    #TODO: no escribirlo siempre, solo cada X frames
     with open(STATUS_FILE, "w") as f:
         f.write(str(time.time()))
     
@@ -117,9 +117,11 @@ def on_new_sample(sink,user_data):
 def main(config):
     global ruta_frames
     global nombre_canal
+    global STATUS_FILE
 
     ruta_frames=config["CAPTURA"]["ruta_frames"]
     nombre_canal=config["CAPTURA"]["nombre_canal"]
+    STATUS_FILE=config["CAPTURA"]["archivo_notificacion_servicio_systemd"]
 
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
@@ -127,28 +129,22 @@ def main(config):
     channel.confirm_delivery()
 
     os.makedirs("frames", exist_ok=True)
-    # Initialize GStreamer
+    
     Gst.init(None)
 
-    # Create the pipeline
     pipeline = Gst.parse_launch(
         config["CAPTURA"]["pipeline_gstreamer"]
     )
 
-    # Get the appsink element
     appsink = pipeline.get_by_name("sink")
 
-    # Set the appsink to emit signals
     appsink.set_property("emit-signals", True)
     appsink.set_property("sync", False)
 
-    # Connect the new-sample signal to the callback
     appsink.connect("new-sample", on_new_sample,channel)
 
-    # Start the pipeline
     pipeline.set_state(Gst.State.PLAYING)
 
-    # Run the main loop
     loop = GLib.MainLoop()
     try:
         loop.run()
@@ -159,8 +155,6 @@ def main(config):
         loop.quit()
         pipeline.set_state(Gst.State.NULL)
         
-
-# Run the script
 if __name__ == "__main__":
 
     parser.add_argument('archivo_configuracion')
