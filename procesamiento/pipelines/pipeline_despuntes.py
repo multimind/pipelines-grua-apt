6e2233eb1b7667102cdb56cfae41eca3f689a4e6
Gonzalo <gonzalo@safetymind.ai>
2024-12-31 09:47:03 -0300

    sube cambios asociados a servidor despuntes

---
+++ procesamiento/pipelines/pipeline_despuntes.py
@@ -0,0 +1,96 @@
+from rx.subject import Subject
+from rx import operators as ops
+from rx import interval
+import numpy as np
+import cv2
+
+import sys
+sys.path.append('../')
+sys.path.append('/home/multimind/sm/safetymind-aceros-arequipa/procesamiento')
+
+from operadores.descarga import factoryStreamRtsp
+from operadores.descarga import factoryStreamMp4
+from operadores.descarga import factoryStreamCarpeta
+from operadores.descarga import factoryStreamPullCarpeta
+from operadores.debug import print_observer
+from operadores.integracion import socket_red_neuronal
+from operadores.medicion import calcular_areas_gruas
+from operadores.dibujo import pintar_grua_apt
+from operadores.deteccion import trabajador_en_zona_grua
+from operadores.debug import guardar_raros
+from operadores.alerta import operador_generar_alerta
+from operadores.limpiar import operador_limpiar
+from operadores.transformar_datos import convertir_boxes_json
+from util import configuracion_logger
+import logging
+import configparser
+import pytz
+from util import util_poligonos
+
+from joblib import dump, load
+from paramiko import SSHClient
+import paramiko
+from datetime import datetime
+import argparse
+import os
+
+def procesar(config):
+
+    nombre_camara="101"
+    ambiente="local"
+
+    configuracion_logger.configurar(nombre_camara,logging.DEBUG, False)
+
+    factory_camara=None
+
+    if config["STREAM"]["tipo"]=="rtsp":
+
+        factory_camara= factoryStreamRtsp.FactoryStreamRtsp(nombre_camara,
+            config["CAMARA"]["url"],
+            config["DESCARGA"]["ruta_descarga"], 
+            config.getint("CAMARA","frameskip"))
+    elif config["STREAM"]["tipo"]=="mp4":
+        factory_camara= factoryStreamMp4.FactoryStreamMp4(nombre_camara,
+            config["VIDEO"]["ruta_video"],
+            config["DESCARGA"]["ruta_descarga"])
+ 
+    elif config["STREAM"]["tipo"]=="carpeta":
+        factory_camara= factoryStreamCarpeta.FactoryStreamCarpeta(nombre_camara,
+            config["STREAM_CARPETA"]["ruta_carpeta"],
+            config["DESCARGA"]["ruta_descarga"])
+    elif config["STREAM"]["tipo"]=="pull_carpeta":
+        factory_camara= factoryStreamPullCarpeta.FactoryStreamPullCarpeta(nombre_camara,
+            config["PULL_CARPETA"]["ruta_carpeta"])
+
+    stream=factory_camara.crear_stream()
+
+    hora_america = pytz.timezone('America/Santiago')
+
+    variables_globales={"cantidad_alertas":0}
+    
+    compuesto=stream.pipe(
+        convertir_boxes_json.convertir(),
+        calcular_areas_gruas.calcular("estructura_imanes",variables_globales,config.getint("GRUA","delta_x"),config.getint("GRUA","delta_y")),
+        trabajador_en_zona_grua.detectar(variables_globales),
+        pintar_grua_apt.pintar(variables_globales,config["DESCARGA"]["pintados"]),
+        #operador_generar_alerta.alerta_imagen(variables_globales,config["TELEGRAM"]["url"],config["TELEGRAM"]["chat_id"],config["DESCARGA"]["pintados"]),
+        guardar_raros.guardar(variables_globales,config["DESCARGA"]["raros"]),
+        operador_limpiar.limpiar(variables_globales,config["DESCARGA"]["pintados"])
+    )
+
+    printObserver=print_observer.PrintObserver()
+
+    compuesto.subscribe(printObserver)
+
+if __name__ == "__main__":
+    os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
+
+    parser = argparse.ArgumentParser(description="Archivo de configu")
+    parser.add_argument("ruta_archivo_configuracion")
+    
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.ruta_archivo_configuracion)
+
+    procesar(config)
---
+++ redes/servidor_pika_despunte.py
@@ -0,0 +1,113 @@
+import pika
+import pickle
+import configparser
+from logging.handlers import RotatingFileHandler
+
+import os
+
+from ultralytics import YOLO
+
+import torch
+import logging
+import argparse
+
+model=None
+ruta_boxes=None
+nombre_canal=None
+
+def log_setup(path, level):
+
+    if not os.path.isdir("logs/despuntes/"):
+            os.makedirs("logs/despuntes/")
+
+    handler = logging.handlers.RotatingFileHandler(path, maxBytes=1024 * 1024,
+                                  backupCount=3)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    
+def inferir_imagen(nombre_imagen, model):
+    global ruta_boxes
+    solo_nombre = os.path.basename(nombre_imagen)
+    solo_nombre = solo_nombre.replace(".jpg", "")
+
+    results = model(nombre_imagen)[0] 
+    boxes = results.boxes.data.tolist()
+    classes = results.names
+
+    respuesta = []
+
+    if len(boxes)==0:
+
+        f = open(ruta_boxes+"/"+solo_nombre, "+w")
+        f.write("")
+        f.close()
+        return
+
+    seleccionados=[]
+    
+    for box in boxes:
+        class_id = box[-1]
+        res = str(classes.get(int(class_id)))+':'+str(int(box[0]))+","+str(int(box[1]))+","+str(int(box[2]))+","+str(int(box[3]))
+
+        confidence = box[4]
+
+        if confidence>0.8:
+            seleccionados.append(res)
+
+    f = open(ruta_boxes+"/"+solo_nombre, "+w")
+       
+    for seleccionado in seleccionados:
+        f.write(seleccionado+"\n")
+    
+    f.close() 
+    
+# Callback for handling messages
+def callback(ch, method, properties, body):
+    global model
+    print(f"Received: {body.decode()}")
+
+    url_frame=body.decode()
+
+    if not os.path.isfile(url_frame):
+        print("archivo no existe: "+url_frame)
+    else:
+        inferir_imagen(url_frame, model)
+    
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+ 
+def procesar(config):
+    global model
+    global ruta_boxes
+    global nombre_canal
+
+    ruta_boxes=config["PROCESAMIENTO"]["ruta_boxes"]
+    nombre_canal=config["PROCESAMIENTO"]["nombre_canal"]
+
+    model = YOLO(config.get("PESOS","ruta"))
+    
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=nombre_canal)
+
+    channel.basic_consume(queue=nombre_canal, on_message_callback=callback)
+
+    channel.start_consuming()
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Inicia el servidor de procesamiento de despuntes')
+    parser.add_argument('archivo_configuracion')
+
+    args = vars(parser.parse_args())
+    archivo_configuracion = args['archivo_configuracion']
+
+    log_setup("logs/despuntes/servidor_despuntes.log", logging.DEBUG)
+
+    config = configparser.ConfigParser()
+    config.read(archivo_configuracion)
+
+    procesar(config)
+
