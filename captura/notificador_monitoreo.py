13fe0444cc31644b6bfd6441c5e7243ed17e162a
Gonzalo <gonzalo@safetymind.ai>
2025-02-14 22:16:26 -0300

    cambia nombre

---
+++ captura/notificador_monitoreo.py
@@ -0,0 +1,41 @@
+import pika
+import json
+import time
+
+RABBITMQ_HOST = 'localhost' 
+QUEUE_NAME = 'reportar_monitoreo'
+
+class NotificadorMonitoreo:
+    def __init__(self,segundos_entre_llamadas,mensaje):
+        self.segundos_entre_llamadas=segundos_entre_llamadas
+        self.tiempo_anterior=time.time()
+        self.mensaje=mensaje
+
+    def notificar_monitoreo_rabbit():
+
+        tiempo_actual=time.time()
+
+        transcurrido = tiempo_actual - self.tiempo_anterior
+
+        if transcurrido<self.segundos_entre_llamadas:
+            return
+
+        self.tiempo_anterior=time.time()
+
+        try:
+            # Connect to RabbitMQ
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+            channel = connection.channel()
+
+            # Declare the queue (idempotent operation)
+            channel.queue_declare(queue=QUEUE_NAME)
+
+            # Publish the message
+            channel.basic_publish(exchange='', routing_key=QUEUE_NAME, body=json.dumps(self.mensaje))
+
+            # Close the connection
+            connection.close()
+
+        except Exception as e:
+            print(f"[!] Error: {e}")
+
